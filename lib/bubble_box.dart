0394d20f0f1f9e72ada9b7bb407d7f172cb01a34
hamzaj9 <98597861+hamzaj9@users.noreply.github.com>
2023-08-17 09:55:54 +0300

    Update bubble_box.dart

--- lib/bubble_box.dart
+++ lib/bubble_box.dart
@@ -1,5 +1,5 @@
 library bubble_box;
 
-export 'package:bubble_box/src/bubble_box.dart';
-export 'package:bubble_box/src/shape/bubble_shape_border.dart';
-export 'package:bubble_box/src/util/path_util.dart';
+export 'package:gradient_bubble_box/src/bubble_box.dart';
+export 'package:gradient_bubble_box/src/shape/bubble_shape_border.dart';
+export 'package:gradient_bubble_box/src/util/path_util.dart';
